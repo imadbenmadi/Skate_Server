c96c275aeef88779d37a94033c2771bdf38ab4ff
imadbenmadi <imadbenmadi891@gmail.com>
2024-01-31 14:37:37 +0100

    init the Server

---
+++ Server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+
+
+const app = express();
+app.use(cors({ credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb://127.0.0.1:27017/Skate";
+async function connect_to_db() {
+    await mongoose.connect(mongoDB, {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+    });
+}
+connect_to_db().catch((err) => console.log(err));
+
+
+
+app.listen(3000);
+
+module.exports = app;
