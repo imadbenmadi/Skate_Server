9a34925e47a982419a490d36b27dacf9a67c3ef9
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-02 17:14:17 +0100

    adding type chekking to Register Rout

--- Controllers/RegisterController.js
+++ Controllers/RegisterController.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const { Users, UserActions } = require("../models/Database");
 
-const handleRegister =  async (req, res) => {
+const handleRegister = async (req, res) => {
     try {
         const { FirstName, LastName, Email, Password, Age, Gender } = req.body;
-        if (!FirstName || !LastName || !Email || !Password) {
+        if (!FirstName || !LastName || !Email || !Password || !Gender) {
             return res.status(409).json({ message: "Missing Data" });
+        } else if (Password.length < 8) {
+            return res.status(409).json({
+                error: "Password must be at least 8 characters",
+            });
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(Email)) {
+            return res.status(409).json({ error: "Invalid Email" });
+        } else if (Gender !== "male" || Gender !== "female") {
+            return res
+                .status(409)
+                .json({
+                    error: "Invalid Gender , accepted values : male or female",
+                });
         }
         const existingUser = await Users.findOne({ Email: Email });
         if (existingUser) {
@@ -23,7 +35,7 @@ const handleRegister =  async (req, res) => {
             res.status(200).json({ message: "Account Created Successfully" });
         }
     } catch (err) {
-         res.status(400).json({ err });
+        res.status(400).json({ err });
     }
 };
 
