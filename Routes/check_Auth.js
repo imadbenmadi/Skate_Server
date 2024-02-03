65859d2af96c8760cf6f9d4ab34f7bccc8095be8
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-03 16:41:36 +0100

    commenting unessary part of the Check_Auth

--- Routes/check_Auth.js
+++ Routes/check_Auth.js
@@ -6,8 +6,6 @@ const verifyToken = require("../Middleware/verifyJWT");
 require("dotenv").config();
 
 router.get("/", async (req, res) => {
-    
-
     try {
         const token = req.cookies.accessToken;
         // console.log("acesstoken to verify :" , token);
@@ -24,50 +22,48 @@ router.get("/", async (req, res) => {
                 if (err) {
                     if (err.name === "TokenExpiredError") {
                         // If the access token is expired, try to refresh it
-                        try {
-                            
-                            
-                            const refreshResponse = await axios.post(
-                                "http://localhost:3000/Refresh", // Adjust the refresh endpoint URL
-                                {},
-                                {
-                                    withCredentials: true,
-                                    validateStatus: () => true,
-                                    headers: {
-                                        Cookie: `refreshToken=${encodeURIComponent(
-                                            req.cookies.refreshToken
-                                        )};`, 
-                                    },
-                                }
-                            );
-                            console.log(refreshResponse.data);
+                        // try {
+                        //     const refreshResponse = await axios.post(
+                        //         "http://localhost:3000/Refresh", // Adjust the refresh endpoint URL
+                        //         {},
+                        //         {
+                        //             withCredentials: true,
+                        //             validateStatus: () => true,
+                        //             headers: {
+                        //                 Cookie: `refreshToken=${encodeURIComponent(
+                        //                     req.cookies.refreshToken
+                        //                 )};`,
+                        //             },
+                        //         }
+                        //     );
+                        //     console.log(refreshResponse.data);
 
-                            if (
-                                refreshResponse.data &&
-                                refreshResponse.data.accessToken
-                            ) {
-                                return res.status(200).json({
-                                    message:
-                                        "Access token refreshed successfully",
-                                });
-                            } else {
-                                // Handle the case where refreshResponse.data.accessToken is missing
-                                console.error(
-                                    "Error refreshing access token"
-                                );
-                                return res.status(401).json({
-                                    message:
-                                        "Error refreshing access token",
-                                });
-                            }
-                        } catch (refreshErr) {
-                            console.error(refreshErr);
-                            return res.status(403).json({
-                                message:
-                                    "Unable to refresh the access token, both tokens are not valid",
-                                error: refreshErr.message,
-                            });
-                        }
+                        //     if (
+                        //         refreshResponse.data &&
+                        //         refreshResponse.data.accessToken
+                        //     ) {
+                        //         return res.status(200).json({
+                        //             message:
+                        //                 "Access token refreshed successfully",
+                        //         });
+                        //     } else {
+                        //         // Handle the case where refreshResponse.data.accessToken is missing
+                        //         console.error("Error refreshing access token");
+                        //         return res.status(401).json({
+                        //             message: "Error refreshing access token",
+                        //         });
+                        //     }
+                        // } catch (refreshErr) {
+                        //     console.error(refreshErr);
+                        //     return res.status(403).json({
+                        //         message:
+                        //             "Unable to refresh the access token, both tokens are not valid",
+                        //         error: refreshErr.message,
+                        //     });
+                        // }
+                        return res.status(401).json({
+                            message: "invalid token",
+                        });
                     }
 
                     // Other types of errors (not related to token expiration)
