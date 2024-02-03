1b5a67970c7ef1d49a663278c3f0b2787e29684d
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-03 21:46:33 +0100

    fixing Logout rout

--- Controllers/LogoutController.js
+++ Controllers/LogoutController.js
@@ -3,39 +3,80 @@ require("dotenv").config();
 const { Users, Refresh_tokens } = require("../models/Database");
 
 const mongoose = require("mongoose");
+
 const handleLogout = async (req, res) => {
-    // On client, also delete the accessToken
+    // On the client, also delete the accessToken
 
-    const cookies = req.cookies;
-    console.log(cookies);
-    if (!cookies?.jwt) {
-        console.log("No cookie found");
-        return res.status(204).json({ lmessage: "No cookie found" }); //No content
-    } 
-    const refreshToken = cookies.jwt;
+    const refreshToken = req.cookies.refreshToken;
+    const accessToken = req.cookies.accessToken;
 
-    // Is refreshToken in db?
-    const foundUser = Refresh_tokens.findOne({ token: refreshToken });
-    if (!foundUser) {
-        res.clearCookie("jwt", {
+    if (!refreshToken) {
+        console.log("No Refresh Cookie found");
+        if (accessToken) {
+            res.clearCookie("accessToken", {
+                httpOnly: true,
+                sameSite: "None",
+                secure: true,
+            });
+            return res.status(204).json({ message: "No cookie found" }); // No content
+        }
+
+        // Clear both cookies on the client side
+        res.clearCookie("refreshToken", {
             httpOnly: true,
             sameSite: "None",
             secure: true,
         });
+
+        res.clearCookie("accessToken", {
+            httpOnly: true,
+            sameSite: "None",
+            secure: true,
+        });
+
+        return res.status(204).json({ message: "No cookie found" });
+    }
+
+    // Is refreshToken in db?
+    const foundUser = await Refresh_tokens.findOne({ token: refreshToken });
+
+    if (!foundUser) {
+        // Clear both cookies on the client side
+        res.clearCookie("refreshToken", {
+            httpOnly: true,
+            sameSite: "None",
+            secure: true,
+        });
+
+        res.clearCookie("accessToken", {
+            httpOnly: true,
+            sameSite: "None",
+            secure: true,
+        });
+
         return res.status(204).json({ message: "No cookie found" });
     }
 
     // Delete refreshToken in db
     try {
         await Refresh_tokens.deleteOne({ token: refreshToken });
-        res.clearCookie("jwt", {
+
+        // Clear both cookies on the client side
+        res.clearCookie("refreshToken", {
             httpOnly: true,
             sameSite: "None",
             secure: true,
         });
+
+        res.clearCookie("accessToken", {
+            httpOnly: true,
+            sameSite: "None",
+            secure: true,
+        });
+
         res.status(204).json({ message: "Logged Out Successfully" });
     } catch (err) {
-        console.log(err);
+        console.error(err);
         res.status(500).json({ message: "Internal Server Error", err });
     }
 };
--- Routes/Logout.js
+++ Routes/Logout.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const logoutController = require("../Controllers/LogoutController");
 
-router.get("/", logoutController.handleLogout);
+router.post("/", logoutController.handleLogout);
 
 module.exports = router;
