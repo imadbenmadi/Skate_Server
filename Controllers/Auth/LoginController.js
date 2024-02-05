b9d88959959b9552caf4116c1e894839c4edf612
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-05 17:14:38 +0100

    making acess token stay longoer

--- Controllers/Auth/LoginController.js
+++ Controllers/Auth/LoginController.js
@@ -19,7 +19,7 @@ const handleLogin = async (req, res) => {
             const accessToken = jwt.sign(
                 { userId: user._id },
                 process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: "10s" }
+                { expiresIn: "10m" }
             );
             const refreshToken = jwt.sign(
                 { userId: user._id },
