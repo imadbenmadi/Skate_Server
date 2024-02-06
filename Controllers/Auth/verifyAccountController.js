2702b53c770642e59481abdcc316f8b87763d46e
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-06 17:00:04 +0100

    adding verify account endpoint

---
+++ Controllers/Auth/verifyAccountController.js
@@ -0,0 +1,52 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { Users, email_verification_tokens } = require("../../models/Database");
+
+const handleVerifyAccount = async (req, res) => {
+    try {
+        const { Code } = req.body;
+        if (!Code) {
+            return res.status(409).json({ error: "Missing Data" });
+        }
+
+        const verificationToken = await email_verification_tokens.findOne({
+            token: Code,
+        });
+
+        if (!verificationToken) {
+            return res
+                .status(404)
+                .json({ error: "Verification token not found" });
+        }
+
+        if (verificationToken.expire < new Date()) {
+            return res
+                .status(409)
+                .json({ error: "Verification token has expired" });
+        }
+
+        const user = await Users.findById(verificationToken.userId);
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        // Update user's email verification status
+        user.IsEmailVerified = true;
+        await user.save();
+
+        // Remove the verification token from the database
+        await email_verification_tokens.deleteOne({
+            _id: verificationToken._id,
+        });
+
+        res.status(200).json({ message: "Account Verified Successfully" });
+    } catch (err) {
+        console.error("Error verifying account:", err);
+        res.status(400).json({
+            error: "An error occurred while verifying the account",
+        });
+    }
+};
+
+module.exports = { handleVerifyAccount };
--- Routes/Auth/verify.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const verifyController = require("../../Controllers/Auth/verifyController");
-router.post("/", verifyController.handleVirefy);
-
-module.exports = router;
