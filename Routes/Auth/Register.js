f0b231b3c063aecd9f18cd727f5d16df8922bbbd
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-08 15:00:54 +0100

    Refactor registration attempts handling

--- Routes/Auth/Register.js
+++ Routes/Auth/Register.js
@@ -36,38 +36,32 @@ router.post("/", async (req, res) => {
     ) {
         delete registrationAttempts[ipAddress]; // Unblock the IP address
     }
-
-    try {
-        // Call the RegisterController to handle the registration process
-        const registrationResult = await RegisterController.handleRegister(req, res);
-
-        // If registration is successful, increment registration attempts and check threshold
-            if (registrationAttempts[ipAddress]) {
-                // Increment registration attempts
-                registrationAttempts[ipAddress].attempts++;
-
-                // Check if registration attempts threshold is exceeded
-                if (registrationAttempts[ipAddress].attempts > 3) {
-                    blockIP(ipAddress, 300000); // Block IP address for 5 minutes (300,000 milliseconds)
-                    return res
-                        .status(429)
-                        .json({ error: "Too many registration attempts. Try again later." });
-                }
-            } else {
-                // If IP address is not blocked, set initial registration attempt
-                registrationAttempts[ipAddress] = {
-                    attempts: 1,
-                };
-            }
-        
-
-        // Return the result of the registration attempt
-    } catch (error) {
-        // Handle any errors that occur during registration
-        console.error("Error during registration:", error);
-        return res.status(500).json({ error: "Internal server error." });
+    // If registration is successful, increment registration attempts and check threshold
+    if (registrationAttempts[ipAddress]) {
+        if (registrationAttempts[ipAddress].attempts >= 5) {
+            return res
+                .status(429)
+                .json({ error: "Too many login attempts. Try again later." });
+        }
     }
-});
+    registrationAttempts[ipAddress] = registrationAttempts[ipAddress] || {
+        attempts: 0,
+    };
 
+    registrationAttempts[ipAddress].attempts++;
+
+    // Check if registration attempts threshold is exceeded
+    if (registrationAttempts[ipAddress].attempts > 10) {
+        blockIP(ipAddress, 300000); // Block IP address for 5 minutes (300,000 milliseconds)
+        return res.status(429).json({
+            error: "Too many registration attempts. Try again later.",
+        });
+    }
+
+    // Call the RegisterController to handle the registration process
+    RegisterController.handleRegister(req, res);
+
+    // Return the result of the registration attempt
+});
 
 module.exports = router;
