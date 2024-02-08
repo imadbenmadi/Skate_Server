08cd7f196f485fd39dc8e01c56581ce9e78ff519
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-08 17:12:43 +0100

    Refactor is_email_verified_Controller and is_email_verified route

--- Controllers/Auth/is_email_verified_Controller.js
+++ Controllers/Auth/is_email_verified_Controller.js
@@ -3,13 +3,15 @@ const { Users } = require("../../models/Database");
 const handle_check = async (req, res)=>{
     try {
         const userId = req.body.userId;
+        if (!userId)
+            return res.status(404).json({error:"messing Data"})
         const user = await Users.findById(userId).select("IsEmailVerified");
         if (!user) {
-            res.status(404).json({ error: "User Not Found" });
+           return res.status(404).json({ error: "User Not Found" });
         }
-        res.status(200).json({ IsEmailVerified: user.IsEmailVerified });
+       return res.status(200).json({ IsEmailVerified: user.IsEmailVerified });
     } catch (error) {
-        res.status(500).json({error: "internal server Error"})
+      return res.status(500).json({ error: "internal server Error" });
     }
 }
 module.exports = { handle_check };
--- Routes/Auth/is_email_verified.js
+++ Routes/Auth/is_email_verified.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const is_email_verified_Controller = require("../../Controllers/Auth/is_email_verified_Controller");
 
-router.get("/", is_email_verified_Controller.handle_check);
+router.post("/", is_email_verified_Controller.handle_check);
 
 module.exports = router;
