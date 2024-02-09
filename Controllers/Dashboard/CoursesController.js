21ca93c2b1028f4f5e5299194cc712e0f1de3c42
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-09 23:44:02 +0100

    pushing reject course notification from the dashboard to the user

--- Controllers/Dashboard/CoursesController.js
+++ Controllers/Dashboard/CoursesController.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-const { Courses, request_Course,Users } = require("../../models/Database");
+const { Courses, request_Course, Users } = require("../../models/Database");
 const jwt = require("jsonwebtoken");
 
-const Verify_Admin = require("../../Middleware/Verify_Admin")
+const Verify_Admin = require("../../Middleware/Verify_Admin");
 
 const handle_add_Courses = async (req, res) => {
     const token = req.cookies.admin_accessToken;
@@ -51,7 +51,7 @@ const handle_Accept_course_request = async (req, res) => {
 
         // Remove the request from the database
         await request_Course.deleteMany({ UserId, CourseId });
-        
+
         const Notificatio_ToSend = {
             Type: "course",
             Title: "Course request accepted",
@@ -59,21 +59,18 @@ const handle_Accept_course_request = async (req, res) => {
             Date: new Date(),
         };
         // Add the course to the user's list of courses , adn Notify him
-       const updateOperations = [
-           Users.findByIdAndUpdate(UserId, {
-               $push: { Courses: CourseId },
-           }).exec(),
-           Users.findByIdAndUpdate(UserId, {
-               $push: { Notifications: Notificatio_ToSend },
-           }).exec(),
-       ];
-       await Promise.all(updateOperations);
+        await Users.findByIdAndUpdate(UserId, {
+            $push: {
+                Courses: CourseId,
+                Notifications: Notificatio_ToSend,
+            },
+        }).exec();
 
         res.status(200).json({ message: "Course request accepted." });
     } catch (error) {
         res.status(500).json({ error: "Internal server error." });
     }
-}
+};
 const handle_Reject_course_request = async (req, res) => {
     const token = req.cookies.admin_accessToken;
 
@@ -92,10 +89,25 @@ const handle_Reject_course_request = async (req, res) => {
 
         // Remove the request from the database
         await request_Course.deleteMany({ UserId, CourseId });
+        const Notificatio_ToSend = {
+            Type: "course",
+            Title: "Course request Rejected",
+            Text: "Your request for the course has been Rejected",
+            Date: new Date(),
+        };
 
+        await Users.findByIdAndUpdate(UserId, {
+            $push: {
+                Notifications: Notificatio_ToSend,
+            },
+        }).exec();
         res.status(200).json({ message: "Course request rejected." });
     } catch (error) {
         res.status(500).json({ error: "Internal server error." });
     }
-}
-module.exports = { handle_add_Courses, handle_Accept_course_request, handle_Reject_course_request};
+};
+module.exports = {
+    handle_add_Courses,
+    handle_Accept_course_request,
+    handle_Reject_course_request,
+};
