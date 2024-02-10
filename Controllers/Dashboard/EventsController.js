7cc275ca1b0196fdde348ec5634b16a2782ff479
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-10 12:48:36 +0100

    adding put and delete endpointsq to Dashboard Events

--- Controllers/Dashboard/EventsController.js
+++ Controllers/Dashboard/EventsController.js
@@ -41,4 +41,65 @@ const handle_add_Event = async (req, res) => {
         res.status(500).json({ error: "Internal server error." });
     }
 };
-module.exports = { handle_add_Event };
+const handle_delete_Event = async (req, res) => {
+    const token = req.cookies.admin_accessToken;
+    if (!token)
+        return res.status(401).json({ error: "Unauthorized: Token missing" });
+    if (!Verify_Admin(token))
+        return res.status(401).json({ error: "Unauthorized: Invalid token" });
+    try {
+        const { eventeId } = req.body;
+        if (!eventeId) {
+            return res
+                .status(400)
+                .json({ error: "eventeId fields is required." });
+        }
+        await Events.findByIdAndDelete(eventeId);
+        res.status(200).json({ message: "evente Deleted successfully." });
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+};
+const handle_update_Event = async (req, res) => {
+    const token = req.cookies.admin_accessToken;
+    if (!token)
+        return res.status(401).json({ error: "Unauthorized: Token missing" });
+    if (!Verify_Admin(token))
+        return res.status(401).json({ error: "Unauthorized: Invalid token" });
+    try {
+        const { eventeId, title, description, image, price, category, date } =
+            req.body;
+        if (!eventeId) {
+            return res.status(400).json({ error: "evente ID is required." });
+        }
+        const evente = await Events.findById(eventeId);
+        if (!evente) {
+            return res.status(404).json({ error: "evente not found." });
+        }
+        // Update each field if provided in the request body
+        if (title) {
+            evente.Title = title;
+        }
+        if (description) {
+            evente.Description = description;
+        }
+        if (image) {
+            evente.Image = image;
+        }
+        if (price) {
+            evente.Price = price;
+        }
+        if (category) {
+            evente.Category = category;
+        }
+        if (date) {
+            evente.Date = date;
+        }
+        // Save the updated evente
+        await evente.save();
+        res.status(200).json({ message: "evente updated successfully." });
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error." });
+    }
+};
+module.exports = { handle_add_Event, handle_delete_Event, handle_update_Event };
--- Routes/Dashboard/Events.js
+++ Routes/Dashboard/Events.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const EventsController = require("../../controllers/Dashboard/EventsController");
 router.post("/", EventsController.handle_add_Event);
-
+router.delete("/", EventsController.handle_delete_Event);
+router.put("/", EventsController.handle_update_Event);
 module.exports = router;
