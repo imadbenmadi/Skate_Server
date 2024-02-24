b1b5d81a15d705bdc7795258135f90d93ad4524a
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-24 14:35:33 +0100

    adding pagination

--- Controllers/ServicesController.js
+++ Controllers/ServicesController.js
@@ -4,10 +4,15 @@ require("dotenv").config();
 const Verify_user = require("../Middleware/verify_user");
 const getAllServices = async (req, res) => {
     try {
-        const services = await Services.find();
-      return res.status(200).json(services);
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 20;
+        const totalCount = await Services.countDocuments();
+        const totalPages = Math.ceil(totalCount / limit);
+        const skip = (page - 1) * limit;
+        const services = await Services.find().skip(skip).limit(limit);
+        return res.status(200).json({ totalPages, services });
     } catch (error) {
-      return res.status(500).json({ error: error });
+        return res.status(500).json({ error: error });
     }
 };
 const get_Service_ById = async (req, res) => {
@@ -24,36 +29,52 @@ const get_Service_ById = async (req, res) => {
             return res.status(404).json({ error: "Service not found." });
         }
 
-       return res.status(200).json(Service);
+        return res.status(200).json(Service);
     } catch (error) {
-       return res.status(500).json({ error: error });
+        return res.status(500).json({ error: error });
     }
 };
 const get_Services_By_user_Id = async (req, res) => {
-    const userId = req.params._idd;
-    if (!userId) return res.status(409).json({ error: "Messing Data" });
+    const userId = req.params._id;
+    if (!userId) return res.status(409).json({ error: "Missing Data" });
+
     const isAuth = await Verify_user(req, res);
     if (isAuth.status == false)
         return res.status(401).json({ error: "Unauthorized: Invalid token" });
+
     if (isAuth.status == true && isAuth.Refresh == true) {
         res.cookie("accessToken", isAuth.newAccessToken, {
             httpOnly: true,
             sameSite: "None",
             secure: true,
-            maxAge: 60 * 60 * 1000, // 10 minutes in milliseconds
+            maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
         });
     }
+
     try {
         const user_in_db = await Users.findById(userId).populate("Services");
         if (!user_in_db) {
-            return res.status(401).json({ error: "user not found." });
+            return res.status(404).json({ error: "User not found." });
         }
 
-       return res.status(200).json(user_in_db.Services);
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 20;
+        const totalCount = user_in_db.Services.length;
+        const totalPages = Math.ceil(totalCount / limit);
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+
+        const services = user_in_db.Services.slice(startIndex, endIndex);
+
+        return res.status(200).json({
+            totalPages,
+            services,
+        });
     } catch (error) {
-       return res.status(500).json({ error:error });
+        return res.status(500).json({ error: error.message });
     }
 };
+
 const handle_request_Service = async (req, res) => {
     const { ServiceId, userId } = req.body;
     const accessToken = req.cookies.accessToken;
@@ -95,11 +116,11 @@ const handle_request_Service = async (req, res) => {
             ServiceId: ServiceId,
         });
         await new_request_Service.save();
-      return res
-          .status(200)
-          .json({ message: "Service requested successfully." });
+        return res
+            .status(200)
+            .json({ message: "Service requested successfully." });
     } catch (error) {
-       return res.status(500).json({ error: error });
+        return res.status(500).json({ error: error });
     }
 };
 module.exports = {
